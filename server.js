6251c625cf509f8adf8e166d9c7e16124f1725cc
Michael <griffinvmichael@gmail.com>
2020-05-15 12:59:15 -0700

    change local to current server IP

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 const jsonParser = bodyParser.json();
 const PORT = 3333;
 
-const CART = 'http://localhost:3001/';
-const REVIEWS = 'http://localhost:3002/';
-const CAROUSEL = 'http://localhost:3003/';
-const DETAILS = 'http://localhost:3004/';
-const RECOMMENDED = 'http://localhost:3005/';
+const CART = 'http://3.17.206.247:3001/';
+const REVIEWS = 'http://3.17.206.247:3002/';
+const CAROUSEL = 'http://3.17.206.247:3003/';
+const DETAILS = 'http://3.17.206.247:3004/';
+const RECOMMENDED = 'http://3.17.206.247:3005/';
 
 app.listen(PORT, () => { console.log(`Now listening on port ${PORT}`); });
 
